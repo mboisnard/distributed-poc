68eaae391a2467b1884ddede96e9fdd38a86f1e8
Mathieu Boisnard <m.boisnard@free.fr>
2018-02-12 19:18:47 +0100

    Add project structure

---
+++ poc-agent/src/main/java/com/esgi/poc/agent/AgentApplication.java
@@ -0,0 +1,12 @@
+package com.esgi.poc.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(AgentApplication.class, args);
+    }
+}
