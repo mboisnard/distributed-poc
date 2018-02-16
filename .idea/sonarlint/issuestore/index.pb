8c6a4fcdc980732126b852bee717be65463036ac
Mathieu Boisnard <m.boisnard@free.fr>
2018-02-16 19:52:56 +0100

    Fix Spring Cloud

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-?
-settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
-G
-poc-config/build.gradle,f/f/ffa67f67ab157333aab6a72e0cc44e388404dc72
-<
-build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
--- poc-agent/src/main/java/com/esgi/poc/agent/AgentApplication.java
+++ poc-agent/src/main/java/com/esgi/poc/agent/AgentApplication.java
@@ -2,11 +2,13 @@ package com.esgi.poc.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AgentApplication {
 
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(AgentApplication.class, args);
     }
 }
