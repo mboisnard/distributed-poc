94266116977ee93ca2d039350f008c5eb86eccfb
uk4 <timothee.arnauld1@gmail.com>
2018-02-18 18:34:20 +0100

    Feature metrics receiver

--- poc-agent/src/main/java/com/esgi/poc/agent/AgentApplication.java
+++ poc-agent/src/main/java/com/esgi/poc/agent/AgentApplication.java
@@ -3,12 +3,12 @@ package com.esgi.poc.agent;
 import com.esgi.poc.receiver.lib.core.Core;
 import com.esgi.poc.receiver.lib.core.utils.annotations.EnableAgentApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAgentApplication
+@SpringBootApplication
 public class AgentApplication {
-
     public static void main(String[] args) {
-        Core.run(AgentApplication.class);
+        //Core.run(AgentApplication.class);
         SpringApplication.run(Core.class);
     }
 }
